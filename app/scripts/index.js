ddec7f929c5231e5a559e25ba6fb04406c0ceadc
Gabriel Pages <gbpages@yahoo.com>
2016-02-29 18:30:33 -0500

    worked on form

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,9 +1,10 @@
-// $(function() {
-//     // Get the form.
-//     var form = $('#ajax-contact');
-//
-//     // Get the messages div.
-//     var formMessages = $('#form-messages');
-//
-//     // TODO: The rest of the code will go here...
-// });
+var $ = require('jquery');
+
+$('#submit').on('click',function(event){
+  event.preventDefault();
+  var form =  $('#form').serialize();
+  $.post('https://docs.google.com/forms/d/1LgvtgbQXaayYVLiUT238_gc1I3YBu-vvyman4woPLGk/formResponse', form, function(){
+    // write function here that will happen when post is posted succesfully
+    // geting issue on request header, ask dan 
+  });
+})
